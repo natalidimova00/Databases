0e000cb72827cc53450334e80e1165da8f40ef98
natalidimova00 <natalidimova00@abv.bg>
2025-05-19 11:53:15 +0300

    updated 3rd week code

--- 1. Movies, PC and Ships/MoviesPCShipsQuery3.sql
+++ 1. Movies, PC and Ships/MoviesPCShipsQuery3.sql
@@ -38,3 +38,83 @@ WHERE model IN (SELECT model
 				FROM pc
 				WHERE speed >= 500)
 
+-- 2.
+SELECT *
+FROM laptop
+WHERE speed < ALL (SELECT speed
+				   FROM pc)
+
+-- 3.		
+SELECT DISTINCT model
+FROM (SELECT model, price
+	  FROM pc
+	  UNION ALL
+	  SELECT model, price
+	  FROM laptop
+	  UNION ALL
+	  SELECT model, price
+	  FROM printer) AS AllProducts
+WHERE price >= ALL (SELECT price
+					FROM pc
+					UNION ALL
+					SELECT price
+					FROM laptop
+					UNION ALL
+					SELECT price
+					FROM printer)
+
+-- 4. 
+SELECT DISTINCT maker
+FROM product 
+WHERE model IN (SELECT model
+				FROM printer
+				WHERE color = 'y' AND price <= all (SELECT price
+													FROM printer
+													WHERE color = 'y'))
+
+-- 5.
+SELECT DISTINCT maker
+FROM product
+WHERE model IN (SELECT model
+				FROM pc AS p1
+				WHERE ram <= all (SELECT ram FROM pc)
+				  AND speed >= all (SELECT speed 
+									FROM pc AS p2
+									WHERE p1.ram = p2.ram))
+
+--------------------------------------
+
+-- 1.
+SELECT DISTINCT COUNTRY
+FROM CLASSES
+WHERE NUMGUNS >= ALL (SELECT NUMGUNS 
+					  FROM CLASSES)
+
+-- 2.
+SELECT NAME
+FROM SHIPS
+WHERE CLASS IN (SELECT CLASS
+				FROM CLASSES
+				WHERE BORE = 16)
+
+-- 3.
+SELECT BATTLE
+FROM OUTCOMES
+WHERE SHIP IN (SELECT NAME
+			   FROM SHIPS
+			   WHERE CLASS = 'Kongo')
+
+-- 4. 
+SELECT CLASS
+FROM CLASSES AS c
+WHERE NUMGUNS >= ALL (SELECT NUMGUNS
+					  FROM CLASSES AS c2
+					  WHERE c2.BORE = c.BORE)
+
+-- 5.
+SELECT NAME
+FROM SHIPS AS s
+JOIN CLASSES AS c on s.CLASS = c.CLASS
+WHERE NUMGUNS >= ALL (SELECT NUMGUNS
+					  FROM CLASSES c2
+					  WHERE c2.BORE = c.BORE)
